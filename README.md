91034a888f1bd04140c132f36ee6c67907eded28
jin <jin1391.jeon@samsung.com>
2026-05-28 15:37:30 +0900

    -. first Commit -. File 추가 : READMD

---
+++ README.md
@@ -0,0 +1,3 @@
+#SimilarityChecker
+
+TDD
---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Source: https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+    // Source: https://mvnrepository.com/artifact/org.assertj/assertj-core
+    testImplementation("org.assertj:assertj-core:3.24.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
